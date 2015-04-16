9f0910a4279f4e316bb5d3370e9a31fa32fffb36
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-04-16 21:26:29 +0200

    Added information to statistics page.

--- src/statistics.c
+++ src/statistics.c
@@ -2,10 +2,45 @@
 
 #include "statistics.h"
 
+#include "data.h"
+#include "timing_handler.h"
+
 static Window *s_window;
 static TextLayer *s_text_layer;
 static ScrollLayer *s_scroll_layer;
-static char s_scroll_text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam tellus, fermentu  m quis vulputate quis, vestibulum interdum sapien. Vestibulum lobortis pellentesque pretium. Quisque ultricies purus e  u orci convallis lacinia. Cras a urna mi. Donec convallis ante id dui dapibus nec ullamcorper erat egestas. Aenean a m  auris a sapien commodo lacinia. Sed posuere mi vel risus congue ornare. Curabitur leo nisi, euismod ut pellentesque se  d, suscipit sit amet lorem. Aliquam eget sem vitae sem aliquam ornare. In sem sapien, imperdiet eget pharetra a, lacin  ia ac justo. Suspendisse at ante nec felis facilisis eleifend.";
+
+static char s_scroll_text[1024];
+
+static void settext(void) {
+  char* buffer = s_scroll_text;
+  if (storage.drank_glasses == 0) {
+    buffer += snprintf(buffer, 50, "You drank nothing today.\n");   
+  } else if (storage.drank_glasses == 1) {
+    buffer += snprintf(buffer, 50, "You drank 1 glass today.\n");   
+  } else {
+    buffer += snprintf(buffer, 50, "You drank %d glasses today.\n", storage.drank_glasses);   
+  }
+    
+  time_t next_timestamp;
+  bool scheduled = timing_handler_next(&next_timestamp);
+  if (scheduled) {
+    struct tm *next = localtime(&next_timestamp);
+    buffer += snprintf(buffer, 50, "Timer scheduled for:\n%04d-%02d-%02d %02d:%02d\n", 
+                                   next->tm_year+1900, next->tm_mon+1, next->tm_mday,
+                                   next->tm_hour, next->tm_min);
+    bool snoozed = timing_handler_next_snooze(&next_timestamp);
+    if (snoozed) {
+      next = localtime(&next_timestamp);
+      buffer += snprintf(buffer, 50, "Snooze scheduled for:\n%04d-%02d-%02d %02d:%02d\n", 
+                                     next->tm_year+1900, next->tm_mon+1, next->tm_mday,
+                                     next->tm_hour, next->tm_min);
+    }
+  } else {
+    buffer += snprintf(buffer, 50, "There is no timer scheduled.");
+  }
+
+  text_layer_set_text(s_text_layer, s_scroll_text);
+}
 
 static void initialise_ui(void) {
   s_window = window_create();
@@ -14,8 +49,8 @@ static void initialise_ui(void) {
   GRect bounds = layer_get_bounds(s_window_layer);
   
   s_text_layer = text_layer_create(GRect(0, 0, 144, 1000));
-  text_layer_set_text(s_text_layer, s_scroll_text);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  settext();
+  // text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   GSize max_size = text_layer_get_content_size(s_text_layer);
   text_layer_set_size(s_text_layer, max_size);
 

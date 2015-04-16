edda3c486ef7961df43b8ea364c72e28e3a77fa6
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-04-16 21:17:27 +0200

    Uninstall app from emulator before installing.

--- build_test_install.sh
+++ build_test_install.sh
@@ -5,6 +5,7 @@ pebble build
 if [[ -n "$1" ]]; then
     if [[ "$1" == "aplite" || "$1" == "basalt" ]]; then
         target="--emulator $1"
+        pebble rm $target 0 || true
     else
         target="--phone $1"
         echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;

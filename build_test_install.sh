780733e906efc23014673bea2d787ae907c6607d
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-04-18 20:52:21 +0200

    Added nohup starting of emulator.

--- build_test_install.sh
+++ build_test_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -ex
+start_emulator=false
+
+while [[ -n "$1" && "${1:0:1}" == "-" ]]; do
+  if [[ "$1" == "-s" ]]; then
+    start_emulator=true
+  fi
+  shift
+done
 
 pebble build
 if [[ -n "$1" ]]; then
@@ -9,6 +17,10 @@ if [[ -n "$1" ]]; then
     else
         target="--phone $1"
         echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;
+    fi
+    if [[ "$start_emulator" == "true" ]]; then
+      nohup pebble install $target &
+      sleep 3
     fi
         
     pebble install $target

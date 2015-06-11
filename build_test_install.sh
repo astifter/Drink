f3c5f7d92543b90d0bc89889653cc1278d2839c6
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-06-11 21:22:39 +0200

    Harmonized build script.

--- build_test_install.sh
+++ build_test_install.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 set -ex
-start_emulator=false
-
-while [[ -n "$1" && "${1:0:1}" == "-" ]]; do
-  if [[ "$1" == "-s" ]]; then
-    start_emulator=true
-  fi
-  shift
-done
 
 pebble build
+
 if [[ -n "$1" ]]; then
     if [[ "$1" == "aplite" || "$1" == "basalt" ]]; then
         target="--emulator $1"
-        pebble rm $target 0 || true
     else
         target="--phone $1"
         echo -n "Press [Enter] to install, [Ctrl-C] to exit."; read -n 1 -s;
     fi
-    if [[ "$start_emulator" == "true" ]]; then
-      nohup pebble install $target &
-      sleep 3
-    fi
-        
+
     pebble install $target
     pebble logs $target
 fi
